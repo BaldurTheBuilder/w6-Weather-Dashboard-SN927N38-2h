391492ebdde1f95ff114d912e10bbf7973f462ed
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2022-11-29 13:55:15 -0800

    linked files and scripts, and added third party APIs to HTML (bootstrap and jquery). Also created base format for the site.

---
+++ assets/javascript/script.js
@@ -0,0 +1,23 @@
+var APIKey = "760d07c1f798bf6eb98e9295a20f84b6";
+var city;
+
+//how to make an API call using just the city name:
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+//var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+//fetch(queryURL)
+
+
+
+
+//GIVEN a weather dashboard with form inputs
+
+//WHEN I search for a city I am presented with current and future conditions for that city and that city is added to the search history
+
+//WHEN I view current city weather, I see the city name, the date, an icon representing weather conditions; the temp, humidity, and wind speed.
+
+//WHEN I view the future forecast, I see a 5-day forecast with dates, an icon representation of weather conditions, the temperature, the wind speed, and the humidity.
+
+//WHEN I click on a city in the search history I am again presented with current and future conditions for that city
+
