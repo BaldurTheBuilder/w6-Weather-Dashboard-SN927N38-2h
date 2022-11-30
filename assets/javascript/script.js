aea2c7d9edcdf52503fd00442224f09d8d216389
BaldurTheBuilder <jack.kokcarlson@gmail.com>
2022-11-29 16:57:40 -0800

    webpage layout has reached MVP. ID's are on important HTML items. Next step is actual javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,31 @@
 var APIKey = "760d07c1f798bf6eb98e9295a20f84b6";
-var city;
+var chosenCity = $("#entered-city-name");
+var searchButton = $("#submit-button");
+var ourForm = $("#city-selection-form");
+var currentWeather = $("#current-weather");
+var fiveDayForecast = $("#five-day-forecast");
 
 //how to make an API call using just the city name:
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
-//var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
+//this function actually retrieves the weather information based on our city selection.
+function getWeatherInformation() {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    fetch(queryURL);
+        
+}
 //fetch(queryURL)
 
+//This function is to run when the user pushes submit. It takes the input city and runs our forecast and current weather functions.
+var handleCitySelection = function (event) {
+    event.preventDefault();
+    var selectedName = chosenCity.val();
+    //getweatherinformation
+    //get 5 day forecast
+}
 
 
 
-//GIVEN a weather dashboard with form inputs
 
 //WHEN I search for a city I am presented with current and future conditions for that city and that city is added to the search history
 
